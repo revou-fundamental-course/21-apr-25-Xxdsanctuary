55d824f4dcaca71eb63aed7c23c1dadce0fcbfc9
Steven <stevannat79@gmail.com>
2025-04-30 00:02:18 +0700

    add kalkulasi JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 function konversi() {
+
+//    membuat HTML tidak refresh, kalau tidak kalkulasi di readonly textarea hilang
+    document.getElementById("form").addEventListener('submit', function(event) {event.preventDefault();})
+
+//    definisi variabel yang dipakai
     let nilai_konversi = document.getElementById("konversi-input").value
-    let result_output = (parseInt(nilai_konversi) * 9/5) + 32
-    function hasil() {
-        document.write(result_output);
-    }
+    let nilai_hasil = (parseFloat(nilai_konversi) * 9/5) + 32
+    const detail = document.querySelector("textarea[name='calculate-detail']")
+    const hasil = document.querySelector("textarea[name='result-output']")
+
+    hasil.value = nilai_hasil.toFixed(2) + "°F"
+    console.log(hasil.value)
+
+//    output hasil konversi dengan metode queryselector
+    detail.value = "(" + nilai_konversi + "°C * 9/5) + 32 = " + nilai_hasil.toFixed(2) + "°F"
+    console.log(detail.value)
+
 }
