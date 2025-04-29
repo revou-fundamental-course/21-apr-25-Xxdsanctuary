1de56bfcb373abd91916fea9adeb4800de0b3f08
Steven <stevannat79@gmail.com>
2025-04-29 21:03:37 +0700

    add JS calculation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-document.getElementById("konversi-input").
-
-document.getElementById("result-output").
-
-document.getElementById("calculate-detail").
+function konversi() {
+    let nilai_konversi = document.getElementById("konversi-input").value
+    let result_output = (parseInt(nilai_konversi) * 9/5) + 32
+    function hasil() {
+        document.write(result_output);
+    }
+}
